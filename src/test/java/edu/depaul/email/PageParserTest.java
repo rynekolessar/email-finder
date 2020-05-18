4ad8e7aee0ffbb2f28d982003b303ffbde326a78
Ryne Kolessar <rasselok123@gmail.com>
2020-05-17 21:27:41 -0500

    added tests

---
+++ src/test/java/edu/depaul/email/PageParserTest.java
@@ -0,0 +1,11 @@
+package edu.depaul.email;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class PageParserTest {
+  // TODO: write PageParser tests
+
+}
---
+++ src/test/java/edu/depaul/email/StorageServiceTest.java
@@ -0,0 +1,10 @@
+package edu.depaul.email;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class StorageServiceTest {
+  // TODO: Write StorageService Tests
+}
