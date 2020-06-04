2d8d9de4d5e748a47ba6f38b083540056880a1fb
rynekolessar <ryne.kolessar@gmail.com>
2020-06-03 21:01:28 -0500

    Updated Storage Service test

--- src/test/java/edu/depaul/email/StorageServiceTest.java
+++ src/test/java/edu/depaul/email/StorageServiceTest.java
@@ -5,15 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-/**
- * To test, you need to call addLocation() to set up where the test will write to, then call
- * storeList() and check that the file specified in addLocation() was actually written to.
- * <p>
- * You can also create error situations by passing bad paths to addLocation. storeList() should
- * then produce the expected exception.
- */
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class StorageServiceTest {
-  // TODO: Write StorageService Tests
 
   @Test
   @DisplayName("Tests the constructor")
@@ -22,5 +22,93 @@ public class StorageServiceTest {
     assertNotNull(storage);
   }
 
+  @Test
+  @DisplayName("Tests that an exception is thrown when no email location is given.")
+  void testExceptionNoEmailLocation() {
+    StorageService storageService = new StorageService();
+    Collection<String> collection = new ArrayList<>();
+    assertThrows(EmailFinderException.class, () -> storageService.storeList(StorageService.StorageType.EMAIL, collection));
+  }
 
+  @Test
+  @DisplayName("Tests that an exception is thrown when no goodlinks location is given")
+  void testExceptionNoGoodLinksLocation() {
+    StorageService storageService = new StorageService();
+    Collection<String> collection = new ArrayList<>();
+    assertThrows(EmailFinderException.class, () -> storageService.storeList(StorageService.StorageType.GOODLINKS, collection));
+  }
+
+  @Test
+  @DisplayName("Tests that an exception is thrown when no badlinks location is given")
+  void testExceptionNoBadLinksLocation() {
+    StorageService storageService = new StorageService();
+    Collection<String> collection = new ArrayList<>();
+    assertThrows(EmailFinderException.class, () -> storageService.storeList(StorageService.StorageType.BADLINKS, collection));
+  }
+
+  @Test
+  @DisplayName("Tests that an exception is thrown when trying to write to an invalid location")
+  void testExceptionInvalidLocation() {
+    StorageService storageService = new StorageService();
+
+    Collection<String> collection = new ArrayList<>();
+    collection.add("testemail@gmail.com");
+    collection.add("testemail2@gmail.com");
+
+    String location = "src/test/resources/invalid/test.txt";
+    storageService.addLocation(StorageService.StorageType.EMAIL, location);
+
+    assertThrows(EmailFinderException.class, () -> storageService.storeList(StorageService.StorageType.EMAIL, collection));
+  }
+
+  @Test
+  @DisplayName("Tests the given badlinks location was written to.")
+  void testBadLinksLocation () throws IOException {
+    StorageService storageService = new StorageService();
+
+    Collection<String> collection = new ArrayList<>();
+    collection.add("https://badlink1.com");
+    collection.add("https://badlink2.com");
+
+    String location = "src/test/resources/badLinks.txt";
+    storageService.addLocation(StorageService.StorageType.BADLINKS, location);
+    storageService.storeList(StorageService.StorageType.BADLINKS, collection);
+
+    List<String> badLinks = Files.readAllLines(Paths.get(location), StandardCharsets.US_ASCII);
+    assertEquals(collection, badLinks);
+  }
+
+  @Test
+  @DisplayName("Tests the given email location was written to.")
+  void testEmailLocation () throws IOException {
+    StorageService storageService = new StorageService();
+
+    Collection<String> collection = new ArrayList<>();
+    collection.add("testemail@gmail.com");
+    collection.add("testemail2@gmail.com");
+
+    String location = "src/test/resources/emails.txt";
+    storageService.addLocation(StorageService.StorageType.EMAIL, location);
+    storageService.storeList(StorageService.StorageType.EMAIL, collection);
+
+    List<String> emails = Files.readAllLines(Paths.get(location), StandardCharsets.US_ASCII);
+    assertEquals(collection, emails);
+  }
+
+  @Test
+  @DisplayName("Tests the given goodLinks location was written to.")
+  void testGoodLinksLocation () throws IOException {
+    StorageService storageService = new StorageService();
+
+    Collection<String> collection = new ArrayList<>();
+    collection.add("https://www.google.com");
+    collection.add("https://cdm.depaul.edu");
+
+    String location = "src/test/resources/goodLinks.txt";
+    storageService.addLocation(StorageService.StorageType.GOODLINKS, location);
+    storageService.storeList(StorageService.StorageType.GOODLINKS, collection);
+
+    List<String> goodLinks = Files.readAllLines(Paths.get(location), StandardCharsets.US_ASCII);
+    assertEquals(collection, goodLinks);
+  }
 }
