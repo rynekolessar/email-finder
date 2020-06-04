d248132e240c3419408b215826e8e1b8eb944e1a
rynekolessar <ryne.kolessar@gmail.com>
2020-06-03 20:21:27 -0500

    updated Page Parser tests

--- src/test/java/edu/depaul/email/PageParserTest.java
+++ src/test/java/edu/depaul/email/PageParserTest.java
@@ -2,20 +2,96 @@ package edu.depaul.email;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-/**
- * As mentioned in slack, one easy way to test PageParser is to create Documents the the constructor
- * that takes a String and verify that the parser handles the Documents correctly.
- * <p>
- * String you should consider using:
- * 1. html with no <a> tags
- * 2. html with 1 <a> tag
- * 3. html with several <a> tags
- * 4. relative tags 5. absolute tags
- */
-public class PageParserTest {
-  // TODO: write PageParser tests
+import java.util.Set;
 
+public class PageParserTest {
+  String testPageSeveral = "<!DOCTYPE html>\n" +
+          "<html lang=\"en\">\n" +
+          "<head>\n" +
+          "    <meta charset=\"UTF-8\">\n" +
+          "    <title>Test</title>\n" +
+          "</head>\n" +
+          "<body>\n" +
+          "    <a href=\"https://www.cdm.depaul.edu/Pages/default.aspx\">CDM Website</a>\n" +
+          "    <a href=\"http://invaildurl.com\">Invalid URL</a>\n" +
+          "\n" +
+          "    <p>emailaddress@gmail.com</p>\n" +
+          "    <p>emailaddress2@gmail.com</p>\n" +
+          "</body>\n" +
+          "</html>";
+
+  String testPageOne = "<!DOCTYPE html>\n" +
+          "<html lang=\"en\">\n" +
+          "<head>\n" +
+          "    <meta charset=\"UTF-8\">\n" +
+          "    <title>Title</title>\n" +
+          "</head>\n" +
+          "<body>\n" +
+          "    <a href=\"https://www.cdm.depaul.edu/Pages/default.aspx\">CDM Website</a>\n" +
+          "\n" +
+          "    <p>emailaddress@gmail.com</p>\n" +
+          "</body>\n" +
+          "</html>";
+
+  String emptyTestPage = "<!DOCTYPE html>\n" +
+          "<html lang=\"en\">\n" +
+          "<head>\n" +
+          "    <meta charset=\"UTF-8\">\n" +
+          "    <title>Title</title>\n" +
+          "</head>\n" +
+          "<body>\n" +
+          "\n" +
+          "</body>\n" +
+          "</html>";
+
+
+  @Test
+  @DisplayName("Tests a Document with no emails or links")
+  void testFindEmailsNone() {
+    Document document = Jsoup.parse(emptyTestPage);
+    PageParser parser = new PageParser();
+    Set<String> emailsFound = parser.findEmails(document);
+    assertEquals(0, emailsFound.size());
+  }
+
+  @Test
+  @DisplayName("tests that the correct number of links are found in a document with 1 <a> tag.")
+  void testFindLinksOneTag() {
+    Document document = Jsoup.parse(testPageOne);
+    PageParser parser = new PageParser();
+    Set<String> linksFound = parser.findLinks(document);
+    assertEquals(1, linksFound.size());
+  }
+
+  @Test
+  @DisplayName("tests that the correct number of emails are found in a document with 1 <a> tag.")
+  void testFindEmailsOneTag() {
+    Document document = Jsoup.parse(testPageOne);
+    PageParser parser = new PageParser();
+    Set<String> emailsFound = parser.findEmails(document);
+    assertEquals(1, emailsFound.size());
+  }
+
+  @Test
+  @DisplayName("Tests that the correct number of links are found in a document with several <a> tags.")
+  void testFindLinksSeveralTags() {
+    Document document = Jsoup.parse(testPageSeveral);
+    PageParser parser = new PageParser();
+    Set<String> linksFound = parser.findLinks(document);
+    assertEquals(2, linksFound.size());
+  }
+
+  @Test
+  @DisplayName("Tests that the correct number of emails are found in a document with several <a> tags")
+  void testFindEmailsSeveralTags() {
+    Document document = Jsoup.parse(testPageSeveral);
+    PageParser parser = new PageParser();
+    Set<String> emailsFound = parser.findEmails(document);
+    assertEquals(2, emailsFound.size());
+  }
 }
