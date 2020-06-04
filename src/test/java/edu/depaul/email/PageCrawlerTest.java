afa92e77d5b07a6db5575c4ca9118c6ccb63e604
rynekolessar <ryne.kolessar@gmail.com>
2020-06-03 23:47:34 -0500

    updated pagecrawler tests

--- src/test/java/edu/depaul/email/PageCrawlerTest.java
+++ src/test/java/edu/depaul/email/PageCrawlerTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PageCrawlerTest {
-  final String BAD_URL = "https://www.a;sdlkfjeinguwp.com";
+  final String BAD_URL = "http://www.invaildurl.com";
+  final String TEST_PAGE = "src/test/resources/testPage3.html";
 
   @Test
   @DisplayName("Tests the PageCrawler constructor")
@@ -19,23 +20,23 @@ public class PageCrawlerTest {
     assertNotNull(crawler);
   }
 
-  @ParameterizedTest
+  @ParameterizedTest()
   @DisplayName("Tests that the crawl function stops when the target size is met")
-  @ValueSource(strings = {"https://www.cdm.depaul.edu/", "https://www.stickyminds.com/", "https://www.amazon.com/"})
+  @ValueSource(strings = {"https://www.cdm.depaul.edu/", TEST_PAGE})
   void testCrawlMax(String url) {
     StorageService mockStorage = mock(StorageService.class);
-    PageCrawler crawler = new PageCrawler(mockStorage, 10);
+    PageCrawler crawler = new PageCrawler(mockStorage, 5);
     crawler.crawl(url);
     crawler.report();
-    assertTrue(crawler.getEmails().size() <= 10);
+    assertTrue(crawler.getEmails().size() <= 5);
   }
 
   @Test
   @DisplayName("Tests invalid urls are stored")
-  void testCrawlInvaildUrl() {
+  void testCrawlInvalidUrl() {
     StorageService mockStorage = mock(StorageService.class);
     PageCrawler crawler = new PageCrawler(mockStorage);
-    crawler.crawl(BAD_URL);
+    crawler.crawl(TEST_PAGE);
     crawler.report();
     assertTrue(crawler.getBadLinks().contains(BAD_URL));
   }
